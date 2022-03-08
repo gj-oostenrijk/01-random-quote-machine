ae3a111c20facd1bbed08d26be4af32062737f2c
Gert Jan Oostenrijk <gj-oostenrijk@users.noreply.github.com>
2022-03-08 16:35:29 +0100

    finish styling

--- src/routes/MarkdownPreviewer.jsx
+++ src/routes/MarkdownPreviewer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../App.css';
 import { marked } from 'marked';
 
-const defaultInput = "# Heading 1\n## Heading 2\n[Duck Duck Go](https://duckduckgo.com)\n`nano`\na code block\n1. list item\n>a blockquote\nan image\nand **bolded** text.";
+const defaultInput = "# Heading 1\n## Heading 2\n[Duck Duck Go](https://duckduckgo.com)\n`nano`\na code block\n1. list item\n>a blockquote\n\n![Sharkie](https://drscdn.500px.org/photo/1022302567/m%3D900/v2?sig=83d898fa3aad472c4b824a7a80c1f047c448672db8cc6de1f18e7b0c1fc271f1 'Shark')\n\nand **bolded** text.";
 
 export default class MarkdownPreviewer extends React.Component {
     constructor() {
@@ -29,12 +29,16 @@ export default class MarkdownPreviewer extends React.Component {
                 id="editor"
                 value={this.state.input}
                 onChange={this.handleChange}
-                rows="10" cols="50"
+                rows="20" cols="100"
                 >Hallo?</textarea>
-            <div id="preview" >
-                {marked.parse(this.state.input)}
-            </div>
-
+                <hr />
+                <h1>And see down here our beautiful render👇</h1>
+                <hr/>
+            <div id="preview" dangerouslySetInnerHTML={
+                {
+                    __html:marked.parse(this.state.input)
+                }
+                }></div>
         </div>
       );
     };
