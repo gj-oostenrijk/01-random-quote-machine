0a1ddc8ba0c70d0ee3b97a1a5f93542298a90a3d
Gert Jan Oostenrijk <gj-oostenrijk@users.noreply.github.com>
2022-03-08 12:36:50 +0100

    Setup routers and navigation

--- src/App.js
+++ src/App.js
@@ -1,61 +1,29 @@
 import React from 'react';
 import './App.css';
+import { Outlet, Link } from "react-router-dom";
 
-const quotes = [
-  {
-    text: "I know what you’re thinking, ’cause right now I’m thinking the same thing. Actually, I’ve been thinking it ever since I got here: Why oh why didn’t I take the blue pill?",
-    author: "Cypher"
-  },
-  {
-    text: "Denial is the most predictable of all human responses.",
-    author: "The Architect"
-  },
-  {
-    text: "Choice is an illusion created between those with power and those without.",
-    author: "Merovingian."
-  }
-];
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-    };
-  }
-
-  updateCount() {
-    if (this.state.count === quotes.length-1) {
-      this.setState({
-        count: 0
-      });
-    } else {
-      this.setState((prevState, props) => {
-        return { count: prevState.count + 1 }
-      });
-    }
-  }
-
-  getTweetURL(){
-    return 'https://twitter.com/intent/tweet?text=' + (quotes[this.state.count].text) + ' — ' + (quotes[this.state.count].author);
-  }
-
-
-
-  render() {
-    return (
-      <div id="quote-box">
-        <div id="text">{quotes[this.state.count].text}</div>
-        <div id="author">— {quotes[this.state.count].author}</div>
-
-        <button id="new-quote" onClick={() => this.updateCount()} >Next Quote</button>
-
-        <div>
-          <a id="tweet-quote" href={this.getTweetURL()} target="_blank">Tweet</a>
-        </div>
+export default function App() {
+  return (
+    <div>
+      <div id="navWrapper">
+        <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+            <Link to="/01-Random-Quote-Machine">01 Random Quote Machine</Link>
+            </li>
+            <li>
+            <Link to="/02-Markdown-Previewer">02 Markdown Previewer</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-    );
-  }
+      <div id='bodyWrapper'>
+        <Outlet />
+      </div>
+      
+    </div>
+  );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import RandomQuote from "./routes/RandomQuote";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index path="01-random-quote-machine" element={<RandomQuote />} />
+        <Route path="about" element={<About />} />
+        <Route // for all other url slugs
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />  
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
